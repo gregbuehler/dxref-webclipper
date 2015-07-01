be54e2c6f6353133194308b5def0ef8d9f7f664b
jmccune <justin.mccune@gmail.com>
2015-07-01 16:11:09 -0400

    instrument buttons

--- content_script.js
+++ content_script.js
@@ -2,14 +2,27 @@ var html =
 `<div id="wco-modal-container">
 <div id='wco-modal'>								
 	<div id='wco-content'>
-		<span class="title"> TITLE: </span> <br>
+		<span class='title'> TITLE: </span> <br>
 		<span id='wco-title'></span> <br>
-		<button>Rip Page</button> <br>
-		<button>Rip SelectedContent</button> <br>
-		<button>Highlight Text Content</button> <br>
+		<button data-type='page'>Rip Page</button> <br>
+		<button data-type='sub-section'>Rip SelectedContent</button> <br>
+		<button data-type='selection'>Highlight Text Content</button> <br>
 	</div>		
 </div>
 </div>`;
 
 
-$('body').append(html);
+var $html = $(html);
+
+
+
+$('body').append($html);
+console.log("SIZE: "+$('#wco-modal-container').length);
+console.log("SIZE: "+$('#wco-modal-container button').length);
+
+$('#wco-modal-container button').click(function(e) {
+	var value = $(e.target).attr('data-type');
+	console.log('PRESSED: '+value);
+});
+
+
