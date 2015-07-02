edd6acd9e9fa7ff816cb6e95c0a57435cff09cf5
jmccune <justin.mccune@gmail.com>
2015-07-01 23:02:47 -0400

    Message passing  both ways demonstrated.

--- background.js
+++ background.js
@@ -7,3 +7,67 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	});
  
 });
+
+
+var receiveMessage = function(event) {
+	alert("RECEIVED A MESSGE!");
+	console.log("BACKGROUND RECEIVED MESSAGE: ");
+	console.dir(event);
+
+  	// We only accept messages from ourselves
+  	if (event.source != window)
+    	return;
+
+  	if (event.data.type && (event.data.type == "FROM_PAGE")) {
+	    console.log("Content script received: " + event.data.msgType);    
+	    console.log("Content script received: " + event.data.msg);    
+	    alert("here1111!");
+  	}
+}
+
+
+window.addEventListener("message", receiveMessage, false);
+
+console.log("BACKGROUND INITIALIZED!");
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    console.dir(request);
+
+    if (request.extension != "WCO") {
+    	return;
+    }
+
+    
+    sendResponse({acknowledged: true, msg: request.msg});
+  });
+
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	console.log("TAB TITLE: "+tab.title);
+	console.log("TAB URL: "+tab.url);
+
+	var info = { title: tab.title, url: tab.url};
+
+	var responseCB = function(responseCount,response) {
+		console.log("RECEIVED RESPONSE>> "+responseCount);
+		console.dir(response);
+	}
+
+	var responseCount = 0;
+	var responseHandler = function(response) {
+		responseCount++;
+  		if (responseCB) {
+  			responseCB(responseCount,response);
+  		}
+	}
+
+	setTimeout(function() {
+		chrome.tabs.sendMessage(tab.id, {extension: "WCO", type: "titleInfo", msg: info}, null, responseHandler);
+	},1000);
+
+});
--- content_script.js
+++ content_script.js
@@ -2,27 +2,102 @@ var html =
 `<div id="wco-modal-container">
 <div id='wco-modal'>								
 	<div id='wco-content'>
-		<span class='title'> TITLE: </span> <br>
-		<span id='wco-title'></span> <br>
+		<span class='title-label'> TITLE: </span> <br>
+		<span id="wco-title"></span> <br>
+		<br>
 		<button data-type='page'>Rip Page</button> <br>
 		<button data-type='sub-section'>Rip SelectedContent</button> <br>
 		<button data-type='selection'>Highlight Text Content</button> <br>
+		<button data-type='hide'>DONE</button> <br>
 	</div>		
 </div>
 </div>`;
 
+// =================================================================
+// Supporting JS
+// =================================================================
+var responseCount = 0;
+var browserWrapper = {
 
-var $html = $(html);
+
+	sendMessage : function(msg, type, responseCB) {
+		if (!type) {
+			type = null;
+		}
+		
+		//window.postMessage({ type: "WCO-CONTROLLER", msgtype: type, msg: msg }, "*");
+		chrome.runtime.sendMessage({extension: "WCO", type: type, msg: msg}, function(response) {
+			responseCount++;
+	  		if (responseCB) {
+	  			responseCB(responseCount,response);
+	  		}
+		});
+	}
 
 
 
-$('body').append($html);
-console.log("SIZE: "+$('#wco-modal-container').length);
-console.log("SIZE: "+$('#wco-modal-container button').length);
+};
 
-$('#wco-modal-container button').click(function(e) {
-	var value = $(e.target).attr('data-type');
-	console.log('PRESSED: '+value);
-});
+
+
+
+
+var printResponse=function(responseNumber,data) {
+	console.log("RECEIVED RESPONSE #"+responseNumber);
+	console.dir(data);
+}
+
+var init = function() {
+	var $html = $(html);
+	$('body').append($html);	
+
+	
+
+
+	$('#wco-modal-container button').click(function(e) {
+		var value = $(e.target).attr('data-type');
+		if (value == 'hide') {
+			console.log("DONE>>> HIDING");
+			$('#wco-modal-container').hide();
+			return;
+		}		
+		browserWrapper.sendMessage("pressed "+value,"buttonInfo", printResponse)
+	});
+
+
+	chrome.runtime.onMessage.addListener(
+  		function(request, sender, sendResponse) {
+    	console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    	console.dir(request);
+
+   		if (request.extension != "WCO") {
+    		return;
+    	}	
+
+    	var title = request.msg.title;
+    	console.log("TITLE: "+title);
+    	console.log(">>"+$('#wco-modal-container span.title').size())
+    	$('#wco-title').text(title);
+
+    
+    	sendResponse({acknowledged: true, msg: request.msg});
+  });
+}
+
+
+
+// =================================================================
+// MAIN
+// =================================================================
+if ($('#wco-modal-container').length == 0) {
+	init();
+}
+else {
+	$('#wco-modal-container').show();
+}
+
+
 
 
